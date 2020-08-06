86f4de202588748d0ffdbcd467692c56d7e58476
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-06 11:53:28 +0600

    let const done

---
+++ letconst.js
@@ -0,0 +1,24 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+// numbers = ["Sunny", "Salman Shah", "Omit"];
+console.log(numbers);
+
+let patientName = "Rahim Chacha";
+console.log(patientName);
+patientName = "Fatem Khala";
+console.log(patientName);
+
+let sum = 0;
+// for(var i  = 0; i < 10; i++){
+//     sum = sum + i;
+// }
+// console.log(i);
+
+for(let i  = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
