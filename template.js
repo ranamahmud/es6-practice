ec96562338b3af1e4e2a2741daaf12175af7b24a
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-06 12:12:03 +0600

    template string done

---
+++ template.js
@@ -0,0 +1,22 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName+" is a good boy";
+console.log(fullName);
+
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+console.log(fullName2);
+
+const fullName3 = `${firstName} ${20+50+30} is a good boy`;
+console.log(fullName3);
+
+const multiline = "I love you\n"
+    + "I miss you\n"
+    + "I need you";
+console.log(multiline);
+
+const multiline2 = `I love you
+I miss you
+No. I don't need you 
+Baily road e dorkar nai.`
+
+console.log(multiline2);
